f2d427bc301fb23317fe51b0c6277373b5ad1e1c
Your Name Here <your_email@youremail.com>
2019-06-12 16:56:28 -0500

    Maybe Final Commit

--- src/main/java/edu/handong/csee/java/LSExtraHW.java
+++ src/main/java/edu/handong/csee/java/LSExtraHW.java
@@ -13,11 +13,14 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 public class LSExtraHW {
+	String ls;
 	String input;
 	boolean lisFiles;
 	boolean DoNotSort;
 	boolean size;
 	boolean help;
+	boolean modificationTime;
+	String LS = "ls";
 	
 	public void run(String[] args) throws IOException {
 		Options options = createOptions();
@@ -29,81 +32,121 @@ public class LSExtraHW {
 			}
 		}
 		
-
-		System.out.println("This is the input path(including the name of dicrectory that you want to search): "+input);
+		if (ls.equals(LS)){
+			System.out.println("This is the input path(including the name of dicrectory that you want to search): "+input);
+			System.out.println(" ");
+			
+			File a = new File(input);
+			ArrayList<String> filenames = new ArrayList<String>();
+			ArrayList<String> filenames2 = new ArrayList<String>();
+			ArrayList<Integer> filelength = new ArrayList<Integer>();
+			ArrayList<String> time = new ArrayList<String>();
+			ArrayList<String> start = new ArrayList<String>();
 		
-		File a = new File(input);
-		ArrayList<String> filenames = new ArrayList<String>();
-		
-		int i = 0;
-		for(File file: a.listFiles()) {
-			if(file.isFile()) {
-				filenames.add(file.getName());
-				System.out.println(filenames.get(i));
-				 i++;
-			}
-		}
-
-		
-		   // String tempFileName = toGet.getName();
-		   // fileNames.add(tempFileName);
-		if (DoNotSort) {
-			System.out.println(input+" ls '-f' or 'DoNotSort' option");
-			System.out.println(".");
-			System.out.println("..");
-
-			i = 0;
+			//stupid..!! 
+			int i = 0;
 			for(File file: a.listFiles()) {
 				if(file.isFile()) {
 					filenames.add(file.getName());
-					System.out.println(filenames.get(i));
-					 i++;
+					//System.out.println(filenames.get(i));
+					i++;
 				}
 			}
-			
-			System.out.println(" ");
+		
+		   
+			if (DoNotSort) {
+				System.out.println(input+" ls '-f' or 'DoNotSort' option");
+				System.out.println(".");
+				System.out.println("..");
 
-		} 
+				i = 0;
+				for(File file: a.listFiles()) {
+					if(file.isFile()) {
+						filenames.add(file.getName());
+						System.out.println(filenames.get(i));
+						i++;
+					}
+				}
+			
+				System.out.println(" ");
+
+			} 
 		
 		
-		if (lisFiles) {
-			System.out.println(input+" ls '-a' or 'lisFiles' option");
-			System.out.println(".");
-			System.out.println("..");
+			if (lisFiles) {
+				System.out.println(input+" ls '-a' or 'lisFiles' option");
+				System.out.println(".");
+				System.out.println("..");
 			
-			Collections.sort(filenames);
+				Collections.sort(filenames);
 
-			for(int j=0; j<filenames.size(); j++) {
+				for(int j=0; j<filenames.size(); j++) {
 					if(j%2==0) {
 						System.out.println(filenames.get(j));
 					}
 				}
 			
 
-			System.out.println(" ");
+				System.out.println(" ");
 
-		} 
+			} 
 		
-		
-		
-		if (size) {
-			System.out.println(input+" ls '-h' or 'size' option");
+			//five options -t sort the list of files by modification time. file.getmodified time. time 별로 sort
+			if (modificationTime) {
+				System.out.println(input+" ls '-t' or 'modification' option");
+			
+				//store the time as string to use collections
+				int k=0;
+				for (File file : a.listFiles()) {
+					long modifiedtime = file.lastModified();
+					start.add(Long.toString(modifiedtime));
+					time.add(start.get(k));
+					k++;
+				}
+			
+				Collections.sort(time);
+
+				for (int j = time.size()-1; j >= 0; j--) {
+					for (File file : a.listFiles()) {
+						long modifiedtime2 = file.lastModified();
+						if(Long.parseLong(time.get(j)) == modifiedtime2) {
+							System.out.println(file.getName());
+						}
+					}
+				}
+			
 			System.out.println(" ");
-			//contians 등등!!!!!
-			i = 0;
-			for(File file: a.listFiles()) {
-				System.out.println("The size of"+file.getName()+":"+ file.length());
 			}
+		
+		
+			if (size) {
+				System.out.println(input+" ls '-h' or 'size' option");
+				System.out.println(" ");
+			
+				Collections.sort(filenames);
 			
 			
-			System.out.println(" ");
+				for(int j=0; j<filenames.size(); j = j+2) {
+					for(File file: a.listFiles()) {
+						if(file.getName().equals(filenames.get(j))) {
+							filenames2.add(filenames.get(j));	
+							filelength.add((int) file.length());
+						}
+					}
+				}
+			
+				for(int j=0; j<filenames.size()/2; j++) {
+					System.out.println("The size of "+filenames2.get(j)+":"+ filelength.get(j));
+				}
+			
+				System.out.println(" ");
+			}
+			}
+		
+		
+		
+		
 		}
-		
-		
-		
-		
-		
-	}
 	
 	private boolean parseOptions(Options options, String[] args) {
 		CommandLineParser parser = new DefaultParser();
@@ -111,6 +154,9 @@ public class LSExtraHW {
 		try {
 
 			CommandLine cmd = parser.parse(options, args);
+			
+			//put the word ls
+			ls = cmd.getOptionValue("l");
 			
 			//input path of file
 			input = cmd.getOptionValue("i");
@@ -126,7 +172,10 @@ public class LSExtraHW {
 			size = cmd.hasOption("h");
 			
 			//-H help
-			help = cmd.hasOption("H");			
+			help = cmd.hasOption("H");		
+			
+			//-t modification time 
+			modificationTime = cmd.hasOption("t");		
 
 		} catch (Exception e) {
 			printHelp(options);
@@ -141,7 +190,14 @@ public class LSExtraHW {
 	//Definition Stage
 		private Options createOptions() {
 			Options options = new Options();
-
+			
+			// add options by using OptionBuilder
+			options.addOption(Option.builder("l").longOpt("LS")
+					.desc("put the word 'ls' as requirement ")
+					.hasArg()
+					.argName("LS")
+					.required()
+					.build());
 			// add options by using OptionBuilder
 			options.addOption(Option.builder("i").longOpt("input")
 					.desc("Set an input file path")
@@ -167,6 +223,13 @@ public class LSExtraHW {
 					.desc("print sizes in human readable format")
 					.argName("size")
 					.build());
+			
+			// add options by using OptionBuilder
+			options.addOption(Option.builder("t").longOpt("modificationTime")
+					.desc("psort the list of files by modification time.")
+					.argName("modificationTime")
+					.build());
+						
 			
 			// add options by using OptionBuilder
 			options.addOption(Option.builder("H").longOpt("help")
